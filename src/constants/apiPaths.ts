acb7605a703a9611d36d8b648d935fb5883e2b17
Maksim Kirilko <maksimkirilko@gmail.com>
2020-12-06 01:00:58 +0300

    Add cart functionality

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://uzg1hf1fba.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://uzg1hf1fba.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ppkq0xydde.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://uzg1hf1fba.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://uzg1hf1fba.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://rss-maksim-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
